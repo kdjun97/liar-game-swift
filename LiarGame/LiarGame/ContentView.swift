d0d635cc3018d7caab51d8441d323029785ea1d0
kdjun97 <kdjun97@gmail.com>
2024-01-01 00:15:15 +0900

    init project

---
+++ LiarGame/LiarGame/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LiarGame
+//
+//  Created by 김동준 on 12/31/23
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LiarGame/LiarGame/LiarGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiarGameApp.swift
+//  LiarGame
+//
+//  Created by 김동준 on 12/31/23
+//
+
+import SwiftUI
+
+@main
+struct LiarGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
